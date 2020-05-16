7d8ada975073c5694ec831efea30b8afa6d1c31b
AliAlperSakar <aalpersakar@gmail.com>
2020-05-16 03:17:57 +0300

    Courses Done/Reservations edited

--- SeniorMobileV4/seniormobile/src/pages/Courses.js
+++ SeniorMobileV4/seniormobile/src/pages/Courses.js
@@ -9,9 +9,9 @@
 
 
 import { Dropdown } from 'react-native-material-dropdown';
-import { Header,Button } from 'react-native-elements';
+import { Header, Button } from 'react-native-elements';
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, Platform, FlatList, Image, SafeAreaView,ScrollView  } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity, Platform, FlatList, Image, SafeAreaView, ScrollView } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import CollapsibleList from "react-native-collapsible-list";
 import axios from 'axios';
@@ -24,27 +24,69 @@ export default class Courses extends Component {
     constructor(props) {
         super(props);
         this.state = {
-          tableHead: [],
-          tableTitle: [],
-          tableData: [
-           
-          ],
-          courses:[]
+            tableHead: [],
+            tableTitle: [],
+            tableData: [
+
+            ],
+            courses: [],
+            course: "",
+            timeSlots: [],
+            instructors: []
         }
-      }
-      componentDidMount() {
-        axios.get("http://192.168.1.20:8082/courses")
+
+        this.select = this.select.bind(this);
+        this.selectTimeSlot = this.selectTimeSlot.bind(this);
+
+    }
+
+
+    componentDidMount() {
+        axios.get("http://192.168.1.30:8082/courses")
             .then(response => this.setState({
-                courses: response.data,
-                tableData:[response.data.map((x)=>[x.schedule])]
+                courses: response.data
             }))
 
     }
-      
+
+    selectTimeSlot = (e) => {
+        const Instructors = [];
+        this.state.courses.map((x) => x.schedule == e ? Instructors.push({ name: x.instructor, level: x.level, place: x.place }) : "NOO");
+        this.setState({
+            instructors: Instructors
+        })
+    }
+
+    select = (e) => {
+        if (e != this.state.course) {
+            this.setState({
+                timeSlots: []
+            })
+        }
+        const { courses } = this.state;
+        const selectedCourses = [];
+        courses.map((x) => x.name == e ? selectedCourses.push([x.schedule]) : "NOO");
+        const timeslots = [];
+        courses.map((x) => x.name == e ? timeslots.push({ value: x.schedule }) : "NOO");
+
+        console.log(selectedCourses);
+        this.setState({
+            course: e,
+            tableData: selectedCourses,
+            timeSlots: timeslots
+        })
+
+
+    }
+
     render() {
-        console.log(this.state.courses)
-        console.log(this.state.tableData)
+        const { timeSlots, instructors, course } = this.state;
+        console.log(this.state.instructors);
+        console.log(this.state.courses);
+
         let day = [{
+            value: 'Yoga',
+        }, {
             value: 'Zumba',
         }, {
             value: 'Stretching',
@@ -72,51 +114,58 @@ export default class Courses extends Component {
 
         const state = this.state;
         return (
-            
+
             <View style={styles.container}>
                 <Header
                     containerStyle={{ width: '100%', backgroundColor: '#09203f', borderBottomWidth: 0, maxHeight: '5%' }}
                     centerComponent={{ text: 'Courses', style: { color: '#fff', marginBottom: '8%', fontSize: 18 } }}
                 /><ScrollView>
-                <View style={styles.dropdown}>
-                    <Dropdown
-                        label='Choose a Course'
-                        data={day} />
-                </View>
-                <View style={styles.container2}>
-                <CollapsibleList
-                        numberOfVisibleItems={1}
-                        wrapperStyle={styles.wrapperCollapsibleList}
-                        buttonContent={
-                            <View style={styles.button}>
-                                <Text style={styles.buttonText}><Icon name="chevron-down" style={styles.icons}/></Text>
-                            </View>
-                        }
-                    >
-                        <View style={styles.collapsibleItem}>
-                            <Text style={styles.announcement}><Icon name="chevron-circle-right" style={styles.icons} /> Course Name / Level
- </Text>
-                        </View>
-                        <View style={styles.collapsibleItem}>
-                            <Text>Instructor : </Text>
-                        </View>
-                        <View style={styles.collapsibleItem}>
-                            <Text>Place :
-</Text>
-                        </View>
-                        <View style={styles.collapsibleItem}>
-                       
-                            
-                               {/* <Text>Schedule </Text><Button title='Enroll' name="Enroll" buttonStyle={styles.enrollButtonStyle}></Button> */}
-                               <Table borderStyle={{borderWidth: 1}}>
-          <Row data={state.tableHead} flexArr={[1, 2, 1, 1]} style={styles.head} textStyle={styles.text}/>
-          <TableWrapper style={styles.wrapper}>
-            <Col data={state.tableTitle} style={styles.title} heightArr={[28,28]} textStyle={styles.text}/>
-            <Rows data={state.tableData} flexArr={[1, 1, 1]} style={styles.row} textStyle={styles.text}/>
-          </TableWrapper>
-        </Table>
-                        </View>
-                    </CollapsibleList></View>
+                    <View style={styles.dropdown}>
+                        <Dropdown
+                            label='Choose a Course'
+                            data={day}
+                            onChangeText={this.select} />
+                        <Dropdown
+                            label='Choose a Time Slot'
+                            data={timeSlots == [] ? [{ value: "Choose a Course" }] : timeSlots}
+                            onChangeText={this.selectTimeSlot} />
+                    </View>
+                    <View style={styles.container2}>
+                        {instructors.map((x) =>
+                            <CollapsibleList
+                                numberOfVisibleItems={1}
+                                wrapperStyle={styles.wrapperCollapsibleList}
+                                buttonContent={
+                                    <View style={styles.button}>
+                                        <Text style={styles.buttonText}><Icon name="chevron-down" style={styles.icons} /></Text>
+                                    </View>
+                                }
+                            >
+                                <View style={styles.collapsibleItem}>
+                                    <Text style={styles.announcement}><Icon name="chevron-circle-right" style={styles.icons} /> {course} / {x.level}
+                                    </Text>
+                                </View>
+                                <View style={styles.collapsibleItem}>
+                                    <Text>Instructor : {x.name}</Text>
+                                </View>
+                                <View style={styles.collapsibleItem}>
+                                    <Text>Place : {x.place}
+                                    </Text>
+                                </View>
+                                <View style={styles.collapsibleItem}>
+
+
+                                    {/* <Text>Schedule </Text><Button title='Enroll' name="Enroll" buttonStyle={styles.enrollButtonStyle}></Button> */}
+                                    <Table borderStyle={{ borderWidth: 1 }}>
+                                        <Row data={state.tableHead} flexArr={[1, 2, 1, 1]} style={styles.head} textStyle={styles.text} />
+                                        <TableWrapper style={styles.wrapper}>
+                                            <Col data={state.tableTitle} style={styles.title} heightArr={[28, 28]} textStyle={styles.text} />
+                                            <Rows data={state.tableData} flexArr={[1, 1, 1]} style={styles.row} textStyle={styles.text} />
+                                        </TableWrapper>
+                                    </Table>
+                                </View>
+                            </CollapsibleList>)}
+                    </View>
                 </ScrollView>
             </View>
 
@@ -129,21 +178,21 @@ const styles = StyleSheet.create({
     container: {
         flex: 1
     },
-   container2:{
-      
-       width:'90%',
-       marginLeft: '5%',
-     
- 
-   
-   },
-   
-   enrollButtonStyle:{
-        width:'18%',
-        backgroundColor:"#09203f",
-       
+    container2: {
 
-   },
+        width: '90%',
+        marginLeft: '5%',
+
+
+
+    },
+
+    enrollButtonStyle: {
+        width: '18%',
+        backgroundColor: "#09203f",
+
+
+    },
     dropdown: {
         width: '90%',
         marginTop: '6%',
@@ -157,15 +206,15 @@ const styles = StyleSheet.create({
         overflow: "hidden",
         backgroundColor: "#FFF",
         borderRadius: 10,
-     
-       
+
+
     },
     collapsibleItem: {
-        borderBottomWidth:StyleSheet.hairlineWidth,
+        borderBottomWidth: StyleSheet.hairlineWidth,
         borderColor: "#CCC",
         padding: 10,
 
-     
+
 
     },
 
@@ -190,7 +239,7 @@ const styles = StyleSheet.create({
     icons: {
         color: "red",
         fontSize: 15,
-       
+
     }
 
 
--- SeniorMobileV4/seniormobile/src/pages/Reservations.js
+++ SeniorMobileV4/seniormobile/src/pages/Reservations.js
@@ -7,65 +7,242 @@
  */
 
 import React, { Component } from 'react';
-import { StyleSheet,AppRegistry, View, Text, TouchableOpacity, Platform, FlatList, Image, SafeAreaView,ScrollView  } from 'react-native';
-import {Header} from 'react-native-elements';
+import { StyleSheet, AppRegistry, View, Text, TouchableOpacity, Platform, FlatList, Image, SafeAreaView, ScrollView } from 'react-native';
+import { Header } from 'react-native-elements';
 import { Dropdown } from 'react-native-material-dropdown';
+import axios from 'axios';
+import CollapsibleList from "react-native-collapsible-list";
 
+
+const reservationtype = [{
+    value: 'Tournaments',
+}, {
+    value: 'Sport Courts',
+}, {
+    value: 'Appointments',
+}, {
+    value: 'Pool',
+},
+];
+
+const tournaments = [{
+    value: "Basketball",
+}, {
+    value: "Football",
+}, {
+    value: "Tennis",
+}, {
+    value: "Racket Sports"
+}];
+
+const time = [{
+    value: '10.40 - 11.30',
+}, {
+    value: '11.40 - 12-30',
+}, {
+    value: '13.40 - 14.30',
+},
+];
+const courts = [{
+    value: 'Football',
+}, {
+    value: 'Basketball',
+}, {
+    value: 'Volleyball',
+}, {
+    value: "Tennis",
+}, {
+    value: "Squash"
+}];
+
+const campus = [{
+    value: "Main"
+}, {
+    value: "East"
+}]
+
+const lanes = [{
+    value: "Lane 1",
+}, {
+    value: "Lane 2",
+}, {
+    value: "Lane 3",
+}, {
+    value: "Lane 4",
+}, {
+    value: "Lane 5",
+}, {
+    value: "Lane 6",
+}]
 
 export default class Reservations extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tableHead: [],
+            tableTitle: [],
+            tableData: [
 
+            ],
+            secondPicker: [],
+            courtReservation: "",
+            appointments: [],
+            instructors: [],
+            reservationType: "",
+            courts: [],
+            second: [],
+            pool: [],
+            text2: "",
+            text3: "",
+            third: []
+        }
+
+        // this.select = this.select.bind(this);
+        // this.selectTimeSlot = this.selectTimeSlot.bind(this);
+        this.selectResType = this.selectResType.bind(this);
+    }
+
+
+    componentDidMount() {
+        axios.get("http://192.168.1.30:8082/courses")
+            .then(response => this.setState({
+                courses: response.data
+            }))
+
+    }
+
+    selectResType = (e) => {
+        const type = e.toLowerCase().replace(" ", "");
+        let sec = [];
+        let third = [];
+        let textSecond = "";
+        let textThird = "";
+        let slots = [];
+        axios.get("http://192.168.1.30:8082/" + type)
+            .then(response => {
+                if(type == "pool")
+                    response.data.map((x) => {
+                        let obj = {value:''}
+                        obj.value = x.time
+                        slots.push(obj)
+                    })
+                    
+                    this.setState({
+                    secondPicker: response.data
+                    })
+                
+            });
+            console.log(slots)
+        if (type == "tournaments") {
+            sec = campus;
+            textSecond = "Choose a Campus";
+            textThird = "Choose a Sport";
+        }
+        else if (type == "pool") {
+            sec = lanes;
+            textSecond = "Choose a Lane";
+            textThird = "Choose a Time Slot ";
+            third = slots
+        }
+
+        this.setState({
+            reservationType: type,
+            second: sec,
+            third: third,
+            text2: textSecond,
+            text3: textThird
+        })
+    }
+
+    selectCampus = (e) => {
+        const camp = e.toLowerCase();
+        // let courtsAvailable = [];
+        // if(camp == "main"){
+        //     courtsAvailable = courts.map((x)=> console.log(x)
+        //     );
+        // }
+        this.setState({
+            campus: e.toLowerCase()
+        })
+    }
+
+    selectCourt = (e) => {
+
+    }
     render() {
+        const { second, third, text2, text3, secondPicker } = this.state;
+        // let unique = [...new Set(third)];
+        // console.log("UNIQ");
+        // console.log(unique);
+        console.log(this.state.secondPicker);
 
-        let reservationtype = [{
-            value: 'Tournaments',
-        }, {
-            value: 'Court Reservations',
-        }, {
-            value: 'Appointments',
-        }, {
-            value: 'Pool',
-        }, 
-        ];
-        let time = [{
-            value: '10.40 - 11.30',
-        }, {
-            value: '11.40 - 12-30',
-        }, {
-            value: '13.40 - 14.30',
-        }, 
-        ];
-        let course = [{
-            value: 'Football',
-        }, {
-            value: 'Basketball',
-        }, {
-            value: 'Volleyball',
-        }];
+
+
         return (
             <View style={styles.container}>
-         
-             <Header
-        containerStyle={{ width: '100%', backgroundColor: '#09203f', borderBottomWidth: 0,maxHeight:'5%' }}
-centerComponent={{ text: 'Reservations', style: { color: '#fff',marginBottom:'7%', fontSize:18} }}
 
-/>
-<View style={styles.dropdown}>
-                <Dropdown
-                        label='Reservation Type'
+                <Header
+                    containerStyle={{ width: '100%', backgroundColor: '#09203f', borderBottomWidth: 0, maxHeight: '5%' }}
+                    centerComponent={{ text: 'Reservations', style: { color: '#fff', marginBottom: '7%', fontSize: 18 } }}
+
+                />
+                <View style={styles.dropdown}>
+                    <Dropdown
+                        label={'Reservation Type'}
                         data={reservationtype}
-                    />
-                    
-                    <Dropdown
-                        label='Choose a Course'
-                        data={course}
+                        onChangeText={this.selectResType}
                     />
                     <Dropdown
-                        label='Choose Time slot'
-                        data={time}
+                        label={text2}
+                        data={second}
+                        onChangeText={this.selectCampus}
                     />
-                    
+                    <Dropdown
+                        label={text3}
+                        data={third}
+                        onChangeText={this.selectCourt}
+                    />
+
 
                 </View>
+                
+                {/* <View style={styles.container2}>
+                                                
+                            <CollapsibleList
+                                numberOfVisibleItems={1}
+                                wrapperStyle={styles.wrapperCollapsibleList}
+                                buttonContent={
+                                    <View style={styles.button}>
+                                        <Text style={styles.buttonText}><Icon name="chevron-down" style={styles.icons} /></Text>
+                                    </View>
+                                }
+                            >
+                                <View style={styles.collapsibleItem}>
+                                    <Text style={styles.announcement}><Icon name="chevron-circle-right" style={styles.icons} /> Lane: / {x.level}
+                                    </Text>
+                                </View>
+                                <View style={styles.collapsibleItem}>
+                                    <Text>Instructor : {x.name}</Text>
+                                </View>
+                                <View style={styles.collapsibleItem}>
+                                    <Text>Place : {x.place}
+                                    </Text>
+                                </View>
+                                <View style={styles.collapsibleItem}> */}
+
+
+                                    {/* <Text>Schedule </Text><Button title='Enroll' name="Enroll" buttonStyle={styles.enrollButtonStyle}></Button> */}
+                                    {/* <Table borderStyle={{ borderWidth: 1 }}>
+                                        <Row data={state.tableHead} flexArr={[1, 2, 1, 1]} style={styles.head} textStyle={styles.text} />
+                                        <TableWrapper style={styles.wrapper}>
+                                            <Col data={state.tableTitle} style={styles.title} heightArr={[28, 28]} textStyle={styles.text} />
+                                            <Rows data={state.tableData} flexArr={[1, 1, 1]} style={styles.row} textStyle={styles.text} />
+                                        </TableWrapper>
+                                    </Table>
+                                </View>
+                            </CollapsibleList>
+                            
+                    </View> */}
             </View>
         );
     }
@@ -76,7 +253,12 @@ centerComponent={{ text: 'Reservations', style: { color: '#fff',marginBottom:'7%
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-       
+
+    },container2: {
+
+        width: '90%',
+        marginLeft: '5%',
+
     },
     dropdown: {
         width: '90%',
@@ -84,7 +266,7 @@ const styles = StyleSheet.create({
         marginLeft: '5%',
 
     },
-    
 
-   
+
+
 });
